ceab2866283b782f23f7585bbd7ca726c50ff923
adamianniello <adam.ianniello@gmail.com>
2015-06-27 12:12:13 -0400

    FULLSCREEN UPDATE
    Added fullscreen HTML code and index2

--- js/main.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  // Handler for .ready() called.
-});
-
-$("#span").hover(function() { 
-		  $(this).animate({textShadow: "#aaa 0 0 0"});
-		}, function() { 
-		  $(this).animate({textShadow: "#aaa 0 0 10px"});
-		});
